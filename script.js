e8a23be2e52a760c3b1b266fa5febad92c9d5a1a
RWR <rwr8106@gmail.com>
2023-01-18 18:37:17 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var tempCelsius = 10;
+var toFahrenheit = tempCelsius * 1.8 + 32;
+
+console.log(
+  "Outside my friend’s house it’s " +
+    tempCelsius +
+    "°C, which is " +
+    toFahrenheit +
+    "°F"
+);
